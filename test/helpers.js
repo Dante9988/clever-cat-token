88b8b88330283c6dc88523a9b15c61b24c59f596
Dante9988 <anvar.hadja@hxro.io>
2021-02-11 02:15:29 -0600

    Seed-exchange added

--- test/helpers.js
+++ test/helpers.js
@@ -5,6 +5,10 @@ export const ether = (n) => {
         web3.utils.toWei(n.toString(), 'ether')
     )
 }
+export const wait = (seconds) => {
+    const milliseconds = seconds * 1000
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
 
 export const tokens = (n) => ether(n)
 
